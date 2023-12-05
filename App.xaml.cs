951151fb271daa8587a5d0c19359b9bb76a72d3c
Duy Quang <vanduyquang2020@gmail.com>
2023-12-06 00:53:55 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Windows.Graphics;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace QuanLyNhaKhoa
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            //Window _mWindow = new MainWindow();
+            //_mWindow.Activate();
+            Window mainWindow = new MainWindow();
+            mainWindow.Activate();
+        }
+
+        public static bool SetTitleBarColors(Window window)
+        {
+            if (window is null)
+            {
+                return false;
+            }
+            window.SystemBackdrop = new MicaBackdrop();
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+
+                var titleBar = window.AppWindow.TitleBar;
+                titleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
+                titleBar.SetDragRectangles(new RectInt32[]
+                {
+                    new RectInt32(40, 0, 10000, 48)
+                });
+                // Set active window colors
+                // Note: No effect when app is running on Windows 10 since color customization is not
+                // supported
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(30, 255, 255, 255);
+                titleBar.ButtonPressedBackgroundColor = Color.FromArgb(40, 0, 0, 0);
+
+                // Set inactive window colors
+                // Note: No effect when app is running on Windows 10 since color customization is not
+                // supported.
+                titleBar.InactiveBackgroundColor = Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Views/MakeAppointmentPage.xaml.cs
@@ -0,0 +1,25 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace QuanLyNhaKhoa.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MakeAppointmentPage : Page
+    {
+        // set up blank page
+        public MakeAppointmentPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
