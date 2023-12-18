6afd60f12bb0e45258e798242757d7eb8aaf606c
Duy Quang <vanduyquang2020@gmail.com>
2023-12-18 20:46:17 +0700

    Add List Binding Example

---
+++ ViewModels/AppointmentCustomerViewModel.cs
@@ -0,0 +1,52 @@
+﻿using QuanLyNhaKhoa.Models;
+using System.ComponentModel;
+
+namespace QuanLyNhaKhoa.ViewModels
+{
+    public class AppointmentCustomerViewModel : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private AppointmentCustomerView appointmentCustomerView;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AppointmentCustomerViewModel(int id, string name)
+        {
+            appointmentCustomerView = new AppointmentCustomerView(id, name);
+            Id = appointmentCustomerView.id;
+            Name = appointmentCustomerView.name;
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Views/MakeAppointmentPage.xaml.cs
+++ Views/MakeAppointmentPage.xaml.cs
@@ -1,5 +1,6 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using QuanLyNhaKhoa.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,10 +12,19 @@ namespace QuanLyNhaKhoa.Views
     /// </summary>
     public partial class MakeAppointmentPage : Page
     {
+        public AppointmentCustomerViewModel[] appointmentCustomerViewModels { get; set; }
         // set up blank page
         public MakeAppointmentPage()
         {
             this.InitializeComponent();
+            appointmentCustomerViewModels = new AppointmentCustomerViewModel[5] {
+                new AppointmentCustomerViewModel(20120172, "Vân Duy Quang"),
+                new AppointmentCustomerViewModel(21120231, "Bùi Hoàng Duy"),
+                new AppointmentCustomerViewModel(21120252, "Võ Hoàng Nam Hưng"),
+                new AppointmentCustomerViewModel(21120332, "Đào Cẩm Thanh"),
+                new AppointmentCustomerViewModel(21120347, "Nguyễn Khắc Triệu")
+            };
+            this.DataContext = this.appointmentCustomerViewModels;
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
